e84ef14d35def0bd7f64cd2a643d51b4d96cb007
Guus1311 <guusfk@gmail.com>
2022-12-05 17:18:14 +0100

    merged challenge 1 and 2

--- day5/opdracht1.php
+++ day5/opdracht1.php
@@ -1,51 +1,67 @@
 <?php
-include "../functions.php";
 $file = fopen("input.txt", "r");
 $inputstack = array();
-for($i = 0; $i < 9; $i++){
-array_push($inputstack, fgets($file));
+for ($i = 0; $i < 9; $i++) {
+    array_push($inputstack, fgets($file));
 }
-print_r($inputstack);
 $stacks = array();
-for($i = 1; $i < 34; $i=$i+4){
+for ($i = 1; $i < 34; $i = $i + 4) {
     $singlestack = array();
-    for($j = 7; $j >= 0; $j--){
-        if($inputstack[$j][$i] != ' ' && $inputstack[$j][$i] != '[' && $inputstack[$j][$i] != ']'){
-       array_push($singlestack, $inputstack[$j][$i]);
+    for ($j = 7; $j >= 0; $j--) {
+        if ($inputstack[$j][$i] != ' ' && $inputstack[$j][$i] != '[' && $inputstack[$j][$i] != ']') {
+            array_push($singlestack, $inputstack[$j][$i]);
         }
     }
-   // print_r($singlestack);
-    array_push($stacks,$singlestack);
+    array_push($stacks, $singlestack);
 }
-print_r($stacks);
 fgets($file);
 $instructions = array();
-while(!feof($file)){
+while (!feof($file)) {
     $line = fgets($file);
-    $words = explode(" ",$line);
-    foreach($words as $word){
-        if($word != "move" && $word != "from" && $word != "to"){
+    $words = explode(" ", $line);
+    foreach ($words as $word) {
+        if ($word != "move" && $word != "from" && $word != "to") {
             array_push($instructions, intval($word));
         }
     }
 }
-print_r($instructions);
-for($i = 0; $i < count($instructions); $i=$i+3){
-    $fromstack = $stacks[$instructions[$i+1]-1];
-    $tostack = $stacks[$instructions[$i+2]-1];
-    //print_r($fromstack);
-    //print_r($tostack);
-    for($j = 0;$j < $instructions[$i];$j++){
-    $tostack[] = end($fromstack);
-    array_pop($fromstack);
+//opdracht1
+$stacks1 = $stacks;
+for ($i = 0; $i < count($instructions); $i = $i + 3) {
+    $fromstack = $stacks1[$instructions[$i + 1] - 1];
+    $tostack = $stacks1[$instructions[$i + 2] - 1];
+
+    for ($j = 0; $j < $instructions[$i]; $j++) {
+        $tostack[] = end($fromstack);
+        array_pop($fromstack);
     }
-    $stacks[$instructions[$i+1]-1] = $fromstack;
-    $stacks[$instructions[$i+2]-1] = $tostack;
-
-
+    $stacks1[$instructions[$i + 1] - 1] = $fromstack;
+    $stacks1[$instructions[$i + 2] - 1] = $tostack;
 }
-//print_r($stacks);
-print("eind\n");
-foreach($stacks as $stack){
+print("opdracht 1: ");
+foreach ($stacks1 as $stack) {
+    print(end($stack));
+}
+print("\n");
+//opdracht2
+$stacks2 = $stacks;
+for ($i = 0; $i < count($instructions); $i = $i + 3) {
+    $fromstack = $stacks2[$instructions[$i + 1] - 1];
+    $tostack = $stacks2[$instructions[$i + 2] - 1];
+
+    $tempstack = array();
+    for ($j = 0; $j < $instructions[$i]; $j++) {
+        $tempstack[] = end($fromstack);
+        array_pop($fromstack);
+    }
+    for ($j = 0; $j < $instructions[$i]; $j++) {
+        $tostack[] = end($tempstack);
+        array_pop($tempstack);
+    }
+    $stacks2[$instructions[$i + 1] - 1] = $fromstack;
+    $stacks2[$instructions[$i + 2] - 1] = $tostack;
+}
+print("opdracht 2: ");
+foreach ($stacks2 as $stack) {
     print(end($stack));
 }
